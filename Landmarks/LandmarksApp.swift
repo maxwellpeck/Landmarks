14ed855dcc697d2345a20f5efa2a7d51aabf0e1e
maxwellpeck <maxwell.peck@uvm.edu>
2021-02-19 17:21:20 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Maxwell Peck on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
